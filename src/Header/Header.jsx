f8186a3fd87343fe46db9105c477be94268d2a6f
Sathish Kumar A <sathish252199@gmail.com>
2022-03-08 22:17:37 +0530

    added header

---
+++ src/Header/Header.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import "./header.css";
+import amazon_img from "../Assets/amazon_img.png";
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+
+export const Header = () => {
+  return (
+      <div className="header">
+          <img
+              src={amazon_img}
+              alt=""
+              className="header_logo"
+          />
+
+          <div className="header_search">
+              <input
+                  type="text"
+                  className="header_searchInput"
+              />
+              <SearchIcon className='header_searchIcon'/>
+              {/* Logo */}
+          </div>
+
+          <div className="header_nav">
+              <div className="header_option">
+                  <span className="header_optionLineOne">Hello Guest</span>
+                  <span className="header_optionLineTwo">Sign In</span>
+              </div>
+              <div className="header_option">
+                  <span className="header_optionLineOne">Returns</span>
+                  <span className="header_optionLineTwo">& Orders</span>
+              </div>
+              <div className="header_option">
+                  <span className="header_optionLineOne">Your</span>
+                  <span className="header_optionLineTwo">Prime</span>
+              </div>
+              <div className="header_option">
+                  <span className="header_optionLineOne">Hello Guest</span>
+                  <span className="header_optionLineTwo">Sign In</span>
+              </div>
+              <div className="header_optionBasket">
+                  <ShoppingBasketIcon />
+                  <span className="header_optionLineTwo header_cartValue">0</span>
+              </div>
+          </div>
+      </div>
+  )
+}
